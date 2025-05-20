8f31c1e927d86ec8437df04796e9fe3068f39721
Matthew Campbell <m.campbell92@gmail.com>
2025-05-20 19:07:02 +0200

    replace hardcoded test word w/ children

--- src/components/WordTile.jsx
+++ src/components/WordTile.jsx
@@ -1,15 +1,15 @@
 import ClickSpark from "../animations/ClickSpark";
 
-export default function WordTile() {
+export default function WordTile({ children }) {
   return (
     <>
       <ClickSpark
         sparkColor="oklch(72.3% 0.219 149.579)"
-        sparkSize={20}
-        duration={600}
+        sparkSize={40}
+        duration={300}
       >
         <div className="text-center bg-zinc-200 w-fit h-auto p-4 rounded-md border-4 border-zinc-900 cursor-pointer">
-          <p className="text-zinc-700 text-3xl font-semibold">Word</p>
+          <p className="text-zinc-700 text-3xl font-semibold">{children}</p>
         </div>
       </ClickSpark>
     </>
