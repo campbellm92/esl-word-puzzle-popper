e1afe4d0602e6d064e6d002b50cf4be63731046b
Matthew Campbell <m.campbell92@gmail.com>
2025-05-20 19:06:44 +0200

    add word tile ui

--- src/components/WordPuzzleGame.jsx
+++ src/components/WordPuzzleGame.jsx
@@ -3,5 +3,13 @@ import useFetchWords from "../hooks/useFetchWords";
 
 export default function WordPuzzleGame() {
   const { words } = useFetchWords("../../data/a1-words.json");
-  console.log(words);
+
+  console.log("words:", words);
+
+  return (
+    <div className="flex flex-col md:flex-row gap-2">
+      {Array.isArray(words) &&
+        words.map((word, index) => <WordTile key={index}>{word}</WordTile>)}
+    </div>
+  );
 }
