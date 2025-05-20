922e032ed589729c86a0f4ab7fdbbe0b45471442
Matthew Campbell <m.campbell92@gmail.com>
2025-05-20 19:05:59 +0200

    add onClick to props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-export default function Button({ children }) {
+export default function Button({ children, onClick }) {
   return (
-    <button className="text-xl p-1 border-4 border-zinc-100 rounded-md cursor-pointer hover:bg-zinc-700">
+    <button
+      onClick={onClick}
+      className="text-xl p-1 border-4 border-zinc-100 rounded-md cursor-pointer hover:bg-zinc-700"
+    >
       {children}
     </button>
   );
