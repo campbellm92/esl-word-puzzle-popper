5d404eeddaf08237eb34db866dfe1f553d6c1b6d
Matthew Campbell <m.campbell92@gmail.com>
2025-05-21 14:25:51 +0200

    add flex styles

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -4,7 +4,7 @@ import SlideInContent from "../animations/SlideInContent";
 export default function IntroScreen({ onStart }) {
   return (
     <SlideInContent distance={300} reverse={true}>
-      <div className="flex justify-center text-center w-screen h-auto">
+      <div className="flex justify-center items-center text-center w-screen h-screen">
         <div className="w-1/2 h-auto p-4 rounded-md border-4 border-zinc-100">
           <h1 className="text-3xl font-semibold mb-4">
             Welcome to Word Puzzle Popper!
@@ -12,7 +12,7 @@ export default function IntroScreen({ onStart }) {
           <p className="text-xl mb-4">
             Click on the correct words as they appear on the screen.
           </p>
-          <Button onClick={onStart}>New Game</Button>
+          <Button onClick={onStart}>Play</Button>
         </div>
       </div>
     </SlideInContent>
